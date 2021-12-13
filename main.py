2683bf12346ab34e9f19a79159c53b985983c080
ToddNajera <angel.najera.98@outlook.com>
2021-12-12 19:11:34 -0600

    final proyecto

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ Salidas:
 -Grafica de las dos señales y valor de correlacion entre 0-1
 -valor de la energia relacionada a la señal (autocorrelacion)
 """
+from numpy import float64
 from utilerias.clases import *
 #constantes para el programa
-ruta_audios = os.path.dirname(os.path.abspath(__file__))+"/audios"
+ruta_audios = os.path.dirname(os.path.abspath(__file__))+"\\audios"
 lista_audios = os.listdir(ruta_audios)
 opcion =True
 
@@ -31,17 +32,65 @@ while(opcion):
     print("\t**********CORRELACION Y AUTOCORRELACION**********")
     print("La siguiente es una lista de las señales de audio disponibles para probar \n Seleccione el cual desea probar:")
     
-    for op,automata in enumerate(lista_audios,start=1):
-        print("{0} - {1}".format(op,automata))
+    for op,audios in enumerate(lista_audios,start=1):
+        print("{0} - {1}".format(op,audios))
 
     print ("0 - salir")
     opcion = input("inserte un valor numerico >>")
 
     if opcion !='0': 
-        muestreo , sonido = waves.read(ruta_audios+"/"+lista_audios[int(opcion)-1])
-        signalWAV = audio(sonido)
-        signalWAV.normalizar()
-        signalWAV.plotear()
+        #Creamos nuestra primera señal de audio
+        muestreo1 , sonido1 = waves.read(ruta_audios+"\\"+lista_audios[int(opcion)-1])
+        signalWAV1 = audio(sonido1,lista_audios[int(opcion)-1])
+        signalWAV1.normalizar()
+
+        #Seleccion de Señales para Correlacion Cruzada
+        print("La siguiente es una lista de las señales de audio disponibles para realizar la correlacion\n (si selecciona el mismo audio, generara la auto correlacion) \n Seleccione el cual desea probar:")
+    
+        for op,audios in enumerate(lista_audios,start=1):
+            print("{0} - {1}".format(op,audios))
+
+        print ("0 - salir")
+        op_corr = input("inserte un valor numerico >>")
+
+        #Creamos nuestra primera señal de audio
+        muestreo2 , sonido2 = waves.read(ruta_audios+"\\"+lista_audios[int(op_corr)-1])
+        signalWAV2 = audio(sonido2,lista_audios[int(op_corr)-1])
+        signalWAV2.normalizar()
+
+        #Proceso de Correlacion
+        #correlacion = signalWAV1.correlacion(signalWAV2.wave)
+
+        #Inicia Proceso de Ploteo de las Señales
+        """pyplot.title('Correlación de Señales')
+        pyplot.subplot(311)
+        signalWAV1.plotear("g")
+        pyplot.subplot(312)
+        signalWAV2.plotear("b")
+        pyplot.subplot(313)
+        pyplot.plot(correlacion,"m", label ='Correlacion')
+        pyplot.legend()
+        pyplot.show()
+        """
+
+        
+
+
+        #correlacion_norm , correlacion_signal = signalWAV1.correlacion_cruzada(signalWAV2,0)
+        correlacion_norm , correlacion_signal = signalWAV1.autocorrelacion()
+
+
+
+        print("Correlacion Normalizada " + str(correlacion_norm))
+        pyplot.title('Correlación de Señales')
+        pyplot.subplot(311)
+        signalWAV1.plotear("g")
+        pyplot.subplot(312)
+        signalWAV2.plotear("b")
+        pyplot.subplot(313)
+        pyplot.plot(correlacion_signal,"m", label ='Correlacion')
+        pyplot.legend()
+        pyplot.show()
 
         break
         
--- utilerias/clases.py
+++ utilerias/clases.py
@@ -17,6 +17,7 @@ Salidas:
 import os
 import numpy
 from time import sleep
+from numpy.core.numeric import correlate
 import scipy.io.wavfile as waves
 import matplotlib.pyplot as pyplot
 
@@ -24,41 +25,86 @@ import matplotlib.pyplot as pyplot
 #Clase que usaremos para leer nuestro archivo de audio
 class audio:
     
-    def __init__(self, archivoWAV):
+    def __init__(self, archivoWAV,name):
         """
         Constructor de la clase
         """
-        self.canales = archivoWAV.shape
+        self.name = name
         if len(archivoWAV.shape) == 1 :
-            self.canal = archivoWAV[:]
+            self.wave = archivoWAV[:]
         else:
-            self.canal = archivoWAV[:,len(archivoWAV.shape)]
+            self.wave = archivoWAV[:,len(archivoWAV.shape)]
+        self.amplitud = numpy.max(self.wave)
+        self.longitud = len(self.wave)
 
     def normalizar(self):
         """
         Sirve para tomar el conjunto de datos y normalizar los valores entre 0 y 1 
         esto dividiendo todos nuestros elemetos entre la amplitud maxima de la señal
         """
-        amplitud = numpy.max(self.canal)
-        self.canal = self.canal / amplitud
+        self.wave = self.wave / self.amplitud
         
     
-    def plotear(self):
+    def plotear(self,color):
         """
         Sirve para mostrar una grafica de la señal
         de audio obtenida del archivo WAV
 
         *uso de pyplot*
         """
-        pyplot.figure(1)
-        pyplot.title("signal 1")
-        pyplot.plot(self.canal)
-        pyplot.show()
+        pyplot.plot(self.wave,color, label = 'Señal '+self.name)
+        pyplot.legend()
     
 
-    def correlacion(self, signalWAV, k):
+    def correlacion(self, signalWAV):
         """
         Sirve para realizar la correlacion con una señal (signalWAV)
-        recibe un k que es el retraso que se le otorga a la seguna senal
+
+        *se utiliza el mode = same 
+        *por si se tiene una señal con mayor longitud el proceso solo se realice con el segmento correspodiente
+        corresponde a la operacion
+        la suma de los productos punto a punto de cada señal, la funcion respeta la manera C12 = signal1[n] * signal2[n+1]
         """
-        pass
+        correlacion = numpy.correlate(self.wave,signalWAV,mode="same")
+        return correlacion
+
+    def correlacion_cruzada(self, signalWAV,k):
+        """
+        Sirve para realizar la correlacion con una señal (signalWAV)
+        recibe un k que es el retraso que se le otorga a la seguna señal
+
+        Depende de las señales se realiza la correlacion hasta la señal mas corta
+
+        regresa correlacion nomalizada  y señal resultante
+        """
+        #la correlacion se acota a la señal mas corta
+        if self.longitud > signalWAV.longitud :
+
+            correlacion_norm = 0
+            correlacion_signal = numpy.zeros(signalWAV.longitud,dtype=numpy.float64)
+            for n in enumerate(signalWAV.wave):
+                correlacion_signal[n[0]] = (1/signalWAV.longitud) * numpy.sqrt(numpy.float_power(self.wave[n[0]+k],2) * numpy.float_power(signalWAV.wave[n[0]],2))
+                correlacion_norm = correlacion_norm + (correlacion_signal[n[0]] / (1/signalWAV.longitud) * correlacion_signal[n[0]])
+        else:
+            correlacion_norm = 0
+            correlacion_signal = numpy.zeros(self.longitud,dtype=numpy.float64)
+            for n in enumerate(self.wave):
+                correlacion_signal[n[0]] = (1/self.longitud) * numpy.sqrt(numpy.float_power(self.wave[n[0]],2) * numpy.float_power(signalWAV.wave[n[0]+k],2))
+                correlacion_norm = correlacion_norm + (correlacion_signal[n[0]] / (1/signalWAV.longitud) * correlacion_signal[n[0]])
+
+        return correlacion_norm , correlacion_signal
+
+    def autocorrelacion(self):
+        """
+        La autocorrelacion es la correlacion cruzada hecha sobre la misma señal
+
+        donde el valor que sevuelve la suma es 
+        """
+        autocorrelacion_norm = 0
+        autocorrelacion_signal = numpy.zeros(self.longitud+1,dtype=numpy.float64)
+        
+        for n in enumerate(self.wave):
+                autocorrelacion_signal[n[0]] = numpy.sqrt(numpy.float_power(self.wave[n[0]],2) * numpy.float_power(self.wave[n[0]],2))
+                autocorrelacion_norm = autocorrelacion_norm + (autocorrelacion_signal[n[0]] / (1/self.longitud) * autocorrelacion_signal[n[0]])
+        
+        return autocorrelacion_norm , autocorrelacion_signal
